38d3d8c93173677ca437fc4d2a52f030a9664098
Matt Swieboda <mswiebod@gmail.com>
2020-05-20 20:08:10 -0500

    Adds Stock model, migration, CSV parsing for seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "Parsing company stock CSV"
+stocks = CSV.read("./company_stock_list.csv", headers: true).map do |row|
+  { symbol: row["Symbol"], name: row["Name"] }
+end
+
+puts "Creating Stock records in bulk"
+stocks = Stock.create(stocks)
+
+puts "#{stocks.size} stocks created"
